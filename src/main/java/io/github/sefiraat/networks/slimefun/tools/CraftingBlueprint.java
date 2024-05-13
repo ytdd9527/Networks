6d00e2c6e0deda8ab09922dc2b28403a942c8ef4
ybw0014 <i@guizhanss.com>
2024-05-13 09:51:48 -0400

    feat(CraftingBluePrint): 合成蓝图堆叠检测

--- src/main/java/io/github/sefiraat/networks/slimefun/tools/CraftingBlueprint.java
+++ src/main/java/io/github/sefiraat/networks/slimefun/tools/CraftingBlueprint.java
@@ -2,13 +2,13 @@ package io.github.sefiraat.networks.slimefun.tools;
 
 import io.github.sefiraat.networks.network.stackcaches.BlueprintInstance;
 import io.github.sefiraat.networks.utils.Keys;
-import io.github.sefiraat.networks.utils.StringUtils;
 import io.github.sefiraat.networks.utils.Theme;
 import io.github.sefiraat.networks.utils.datatypes.DataTypeMethods;
 import io.github.sefiraat.networks.utils.datatypes.PersistentCraftingBlueprintType;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.DistinctiveItem;
 import io.github.thebusybiscuit.slimefun4.implementation.items.blocks.UnplaceableBlock;
 import net.guizhanss.guizhanlib.minecraft.helper.inventory.ItemStackHelper;
 import org.bukkit.ChatColor;
@@ -19,7 +19,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CraftingBlueprint extends UnplaceableBlock {
+public class CraftingBlueprint extends UnplaceableBlock implements DistinctiveItem {
 
     public CraftingBlueprint(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
@@ -51,4 +51,8 @@ public class CraftingBlueprint extends UnplaceableBlock {
         blueprint.setItemMeta(itemMeta);
     }
 
+    @Override
+    public boolean canStack(ItemMeta meta1, ItemMeta meta2) {
+        return meta1.getPersistentDataContainer().equals(meta2.getPersistentDataContainer());
+    }
 }
