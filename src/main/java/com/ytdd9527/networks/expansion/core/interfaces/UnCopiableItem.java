27c39d088808bb25d45997a474e545bfe251d6aa
ytdd9527 <2022425155@qq.com>
2024-06-30 21:21:38 +0800

    更新1

--- src/main/java/com/ytdd9527/networks/expansion/core/interfaces/UnCopiableItem.java
+++
@@ -1,5 +0,0 @@
-package com.ytdd9527.networks.expansion.core.interfaces;
-
-
-public interface UnCopiableItem {
-}
--- src/main/java/io/github/sefiraat/networks/network/barrel/BarrelType.java
+++
@@ -1,9 +0,0 @@
-package io.github.sefiraat.networks.network.barrel;
-
-public enum BarrelType {
-    NETWORKS,
-    INFINITY,
-    FLUFFY,
-    STORAGE_UNIT,
-    UNKNOWN
-}
